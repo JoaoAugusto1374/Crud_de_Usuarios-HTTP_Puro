72f049b796a18e029611aaa15efec0fd4e625d12
JoaoAugusto1374 <tauajoaoaugustoce@gmail.com>
2025-06-16 21:57:50 -0300

    feat: ajustando as rotas no app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+from server import Server
+from routes.usuarios import listar_usuarios, novo_usuario, editar_usuario, excluir_usuario
+
+app = Server()
+
+app.route("/usuarios")(listar_usuarios)
+app.route("/usuarios/novo")(novo_usuario)
+app.route("/usuarios/<id>/editar")(editar_usuario)
+app.route("/usuarios/<id>/excluir")(excluir_usuario)
+
+if __name__ == "__main__":
+    app.start()
